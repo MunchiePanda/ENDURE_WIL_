51907b9fab633d9f1a3c3552d821f86d82c0a67e
siobhanButler <siobhanbutler8@gmail.com>
2025-11-24 20:34:06 +0200

    Pause & Death UI added no script logic

---
+++ Assets/2_UI/UI_Scripts/DeathUIManager.cs
@@ -0,0 +1,32 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DeathUIManager : MonoBehaviour
+{
+    public TMP_Text txt_Title;          // "You Died" or "You Win!"
+    public Button btn_Exit;
+    public Button btn_Respawn;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnExitClicked()
+    {
+
+    }
+
+    void OnRespawnClicked()
+    {
+
+    }
+}
